bd697bab9ffe82ffb741cbcf6930df472dd75049
Priceputu Cristian <cristian.priceputu@woowteam.com>
2018-01-30 09:16:15 +0200

    Not yielding. Weird.

--- org/price/multicorn/fs.py
+++ org/price/multicorn/fs.py
@@ -2,15 +2,15 @@ from multicorn import ForeignDataWrapper
 from multicorn.utils import log_to_postgres
 from multicorn.utils import ERROR
 from multicorn.utils import INFO
-import string
-import platform
+
 from org.price.multicorn.fs_utils import walk
+from org.price.multicorn.fs_utils import get_root
+from org.price.multicorn.fs_utils import try_get_root
+from org.price.multicorn.fs_utils import raise_
 from os.path import join
 from os.path import getsize
-
-
-def raise_(ex):
-    raise ex
+import os
+import sys
 
 
 class Fs(ForeignDataWrapper):
@@ -18,35 +18,16 @@ class Fs(ForeignDataWrapper):
         super(Fs, self).__init__(options, columns)
         self.debug = (bool)(options.get('debug')) or False
         if self.debug:
+            log_to_postgres('Python Version: {}'.format(sys.version_info), INFO)
             log_to_postgres('Init {}{}'.format(options, columns), INFO)
 
-    def get_rel_size(self, quals, columns):
-        if self.debug:
-            log_to_postgres('Init {}{}'.format(quals, columns), INFO)
-        return 1, 1000
-
     def execute(self, quals, columns, sortkeys=None):
-        location = self.get_root()
+        for n in range(10):
+            yield {'filename': str(n)}
 
-        columns = sorted(columns)
-        if self.debug:
-            log_to_postgres("Sorted columns: {}".format(columns), INFO)
 
-        for qual in quals:
-            if self.debug:
-                log_to_postgres(qual, INFO)
-            location = self.try_get_location(qual) or location
-
-        if self.debug:
-            log_to_postgres('Selected Location : {}'.format(location), INFO)
-
-        if self.debug:
-            log_to_postgres('Execute: {} {} {}'.format(quals, columns, sortkeys), INFO)
-
-        for root, dirs, files, deeplevel in list(walk(location, onerror=raise_)):
-            if self.debug:
-                log_to_postgres('File {} {} {} '.format(root, dirs, files), INFO)
-
+def nothing():
+    for root, dirs, files, deeplevel in list(walk('/li', onerror=raise_)):
         try:
             for file in files:
                 yield {"depth": deeplevel, "filename": file, "filesize": getsize(join(root, file)), "location": root, "type": 'f'}
@@ -56,30 +37,3 @@ class Fs(ForeignDataWrapper):
 
         except Exception:
             log_to_postgres(str(Exception), ERROR)
-
-    def try_get_location(self, qual):
-        if qual.field_name == 'location':
-            if qual.operator == '=':
-                return qual.value
-
-            log_to_postgres('Unsupported operator {}'.format(qual.operator), ERROR)
-        return None
-
-    def get_root(self):
-        system = platform.system()
-
-        if system.lower() == 'windows':
-            return self.get_drives()[0] + '\\'
-
-        return '/'
-
-    def get_drives(self):
-        from ctypes import windll
-        drives = []
-        bitmask = windll.kernel32.GetLogicalDrives()
-        for letter in string.uppercase:
-            if bitmask & 1:
-                drives.append(letter)
-            bitmask >>= 1
-
-        return drives
--- org/price/multicorn/fs_utils.py
+++ org/price/multicorn/fs_utils.py
@@ -1,5 +1,9 @@
 from os.path import join, isdir, islink
 from os import error, listdir
+import string
+import platform
+from multicorn.utils import log_to_postgres
+from multicorn.utils import ERROR
 
 
 def walk(top, topdown=True, onerror=None, deeplevel=0):
@@ -26,3 +30,37 @@ def walk(top, topdown=True, onerror=None, deeplevel=0):
                 yield x
     if not topdown:
         yield top, dirs, nondirs, deeplevel
+
+
+def get_root():
+    system = platform.system()
+
+    if system.lower() == 'windows':
+        return get_drives()[0] + '\\'
+
+    return '/'
+
+
+def get_drives():
+    from ctypes import windll
+    drives = []
+    bitmask = windll.kernel32.GetLogicalDrives()
+    for letter in string.uppercase:
+        if bitmask & 1:
+            drives.append(letter)
+        bitmask >>= 1
+
+    return drives
+
+
+def try_get_root(qual):
+    if qual.field_name == 'root':
+        if qual.operator == '=':
+            return qual.value
+
+        log_to_postgres('Unsupported operator {}'.format(qual.operator), ERROR)
+    return None
+
+
+def raise_(ex):
+    raise ex
